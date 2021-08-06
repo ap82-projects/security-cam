74e7dd73d3d0d787b502cda6b010ce18d59cbbd1
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-08-06 12:37:31 +0900

    Created basic react app and confirmed connection between client and server

--- server/main.go
+++ server/main.go
@@ -11,6 +11,11 @@ func main() {
 	router.Use(static.Serve("/", static.LocalFile("../client", true)))
 	api := router.Group("/api")
 	api.GET("/")
+	api.GET("/test", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "passed",
+		})
+	})
 
 	router.Run()
 }
