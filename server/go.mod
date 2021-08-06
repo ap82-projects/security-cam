bf2a4accd32a69c6ae1a4c7c3253d6514563fca3
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-08-06 14:08:30 +0900

    testing accessing environment variables in heroku

--- server/go.mod
+++ server/go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19 // indirect
 	github.com/gin-gonic/gin v1.7.3 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./web", true)))
 	api := router.Group("/api")
 	api.GET("/")
 	api.GET("/test", func(context *gin.Context) {
+		message := os.Getenv("TEST")
 		context.JSON(200, gin.H{
-			"message": "passed",
+			"message": message,
 		})
 	})
 
