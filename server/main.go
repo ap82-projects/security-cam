fa49111b1cc02cc76f387731f9a01efcab9687a4
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-08-06 14:16:06 +0900

    Removing .env file access from main.go to test heroku deployment.

--- server/main.go
+++ server/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./web", true)))
