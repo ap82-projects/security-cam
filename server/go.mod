f66ca75c02358dfb60a04538713a008bbbb2fdc6
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-08-07 09:23:46 +0900

    Added POST, GET, and DELETE instructions for the /api/user endpoint

--- server/go.mod
+++ server/go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19 // indirect
 	github.com/gin-gonic/gin v1.7.3 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	google.golang.org/api v0.40.0 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	// "encoding/json"
 	"context"
+	// "fmt"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 
 	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/mitchellh/mapstructure"
 	"google.golang.org/api/option"
 )
 
@@ -39,6 +43,41 @@ func main() {
 	}
 	log.Println(client)
 
+	/////////////////////////////////////////////////////////////////////////////
+	//*************************************************************************//
+	//******************** Structs for interacting with DB ********************//
+	//*************************************************************************//
+	/////////////////////////////////////////////////////////////////////////////
+
+	type UserId struct {
+		Id string
+	}
+	type Incident struct {
+		Time  string
+		Image string
+	}
+	type WatchingUpdate struct {
+		Watching bool
+	}
+	type WatchingMessageToCamera struct {
+		Id       string
+		Watching bool
+	}
+	type User struct {
+		Name      string
+		GoogleId  string
+		Email     string
+		Phone     string
+		Incidents []Incident
+		Watching  bool
+	}
+
+	/////////////////////////////////////////////////////////////////////////////
+	//*************************************************************************//
+	//************************** REST API Endpoints ***************************//
+	//*************************************************************************//
+	/////////////////////////////////////////////////////////////////////////////
+
 	router := gin.Default()
 	router.Use(static.Serve("/", static.LocalFile("./web", true)))
 	api := router.Group("/api")
@@ -48,6 +87,69 @@ func main() {
 		context.JSON(200, gin.H{
 			"message": message,
 		})
+	})
+
+	api.POST("/user", func(c *gin.Context) {
+		log.Println("Request on /api/user type: POST")
+
+		var newUser User
+		err := json.NewDecoder(c.Request.Body).Decode(&newUser)
+		if err != nil {
+			// http.Error(w, err.Error(), http.StatusBadRequest)
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		log.Println("Adding user:")
+		log.Println(newUser)
+
+		fsDocRef, fsWriteResult, err := client.Collection("users").Add(ctx, map[string]interface{}{
+			"name":      newUser.Name,
+			"googleid":  newUser.GoogleId,
+			"email":     newUser.Email,
+			"phone":     newUser.Phone,
+			"incidents": newUser.Incidents,
+			"watching":  newUser.Watching,
+		})
+		log.Println("New user id", fsDocRef.ID, "created at", fsWriteResult)
+		// fmt.Fprintln(w, "{ \"id\":", fsDocRef.ID, "}")
+		c.String(http.StatusOK, "{ \"id\":", fsDocRef.ID, "}")
+		if err != nil {
+			log.Fatalf("Failed adding user: %v", err.Error())
+		}
+	})
+
+	api.GET("/user/:id", func(c *gin.Context) {
+		log.Println("Request on /api/user type: GET")
+		userDocId := c.Param("id")
+		log.Println("Retrieving user with Document ID", userDocId)
+		query, errQ := client.Collection("users").Doc(userDocId).Get(ctx)
+		if errQ != nil {
+			log.Fatal("Error retrieving user data")
+		}
+		var currentUser User
+		mapstructure.Decode(query.Data(), &currentUser)
+		currentUserData, err := json.Marshal(currentUser)
+		if err != nil {
+			log.Println("Error:", err.Error())
+		}
+		log.Println("Sending user data")
+		log.Println(string(currentUserData))
+		c.String(http.StatusOK, string(currentUserData))
+	})
+
+	api.DELETE("/user/:id", func(c *gin.Context) {
+		log.Println("Request on /api/user type: DELETE")
+		userId := c.Param("id")
+
+		log.Println("Deleting user ID", userId)
+		fsDeleteTime, err := client.Collection("users").Doc(userId).Delete(ctx)
+		if err != nil {
+			log.Println("An error has occurred:", err.Error())
+			c.String(http.StatusBadRequest, "An error has occurred:", err.Error())
+		} else {
+			log.Println("User", userId, "deleted at", fsDeleteTime)
+			c.String(http.StatusOK, "User", userId, "deleted at", fsDeleteTime)
+		}
 	})
 
 	router.Run()
